c6a82279ce80842b206cce98d8e0357df9f0aea3
siddhantgedekar <siddhantgedekar.sits.comp@gmail.com>
2026-02-19 16:49:12 +0530

    valid parenthesis in python

--- turing/validBracket.py
+++ turing/validBracket.py
@@ -2,9 +2,21 @@ string = "{[()]}"
 
 def isValid(s):
     stack = []
-    map = {')': '(', '}': '{', ']': '['}
-    # loop through string
-    # check for closing bracket
-    # pop the last el from stack
+
+    for char in s:
+        # append openint brackets to stack
+        if char == '(' or char == '{' or char == '[':
+            stack.append(char)
+        else: # handle closing brackets
+            # if stack empty? means no closing bracket found, hence return false
+            if len(stack) == 0:
+                return False
+            # if closing bracket found
+            if (char == ']' and stack[-1] == '[') or (char == '}' and stack[-1] == '{') or (char == ')' and stack[-1] == '('):
+                stack.pop() # pop it
+            else: # if no closing found
+                return False
+    # if stack == empty, means all brackets are closed, hence return true
+    return len(stack) == 0
 
 print("Is the string valid? ",isValid(string))
