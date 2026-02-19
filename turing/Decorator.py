d786df0102350898ad77bc8e1e3dc8728718c17b
siddhantgedekar <siddhantgedekar.sits.comp@gmail.com>
2026-02-19 15:27:40 +0530

    decorator in python

--- turing/Decorator.py
+++ turing/Decorator.py
@@ -4,4 +4,28 @@
 # Turing Question: Write a decorator that 
 # logs how long a function takes to run.
 
- 
+import time
+
+# a simple time decorator
+
+# factorials
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def time_decorator(func):
+    def wrapper():
+        start = time.time() # start time
+        result = factorial(10) # call the func..
+        end = time.time() # end time
+        print(f"{func.__name__} took {end - start} seconds")
+        return result
+    return wrapper
+
+@time_decorator
+def heavy_computation():
+    time.sleep(1) # sleep a sec
+
+heavy_computation() # call the func..
