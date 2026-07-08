296fa18e93bb49da54f410e2da75b1b77ba1551f
siddhantgedekar <siddhantgedekar.sits.comp@gmail.com>
2026-07-08 08:20:57 +0530

    debug with python and VSCode

--- HackerRank/python/debugging.py
+++ HackerRank/python/debugging.py
@@ -1,7 +1,14 @@
 import pdb
 
+def add(a,b):
+    result = a+b
+    print(f'The sum of {a} and {b} is {result}')
+    return result
+
 def divide(a,b):
     pdb.set_trace()
     return (a/b)
 
 print(divide(10,2))
+print(divide(15, 3))
+add(10,2)
