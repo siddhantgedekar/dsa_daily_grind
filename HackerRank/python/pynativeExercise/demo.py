741a5bd11e89feceab1e211d2d8f59ca78e6ba44
siddhantgedekar <siddhantgedekar.sits.comp@gmail.com>
2026-09-12 09:52:53 +0530

    string balancing in python

---
+++ HackerRank/python/pynativeExercise/demo.py
@@ -0,0 +1,2 @@
+for i in range(0, 765, 90):
+    print(i)
---
+++ HackerRank/python/pynativeExercise/strBalance.py
@@ -0,0 +1,7 @@
+def containsLetter(letters, string):
+    words = words.lower()
+    string = string.lower()
+    
+
+s1 = "yn"
+s2 = "PyNative"
